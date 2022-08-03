b65c3df88ef5a3e747751ac3129a90fdc2ea5094
swanhell <stephen_wanhella@sfu.ca>
2022-08-03 16:04:26 -0700

    initial work

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const gameStyles = {
+  backgroundColor: 'salmon',
+  margin: 10,
+  padding: 20
+}
+
+const Board = () => {}
+
+const Game = () => {
+  return (
+    <div style={gameStyles}>
+      Game
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+)
